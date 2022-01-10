cba68fc66996e43833a94c0f3e232b0e219ba093
Sayandip Halder <sayandiph4@gmail.com>
2022-01-11 02:57:47 +0530

    complete cartitem component with all features

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -12,7 +12,28 @@ type Props = {
 
 const CartItem: React.FC<Props> = ({item, addToCart, removeFromCart}) => (
     <Wrapper>
-        
+        <div>
+            <h3>{item.title}</h3>
+            <div className="information">
+                <p>Price: ₹{item.price}</p>
+                <p>Total: ₹{(item.amount * item.price).toFixed(2)}</p>
+            </div>
+            <div className="buttons">
+                <Button
+                    size='small'
+                    disableElevation
+                    variant='contained'
+                    onClick={() => removeFromCart(item.id)}
+                >-</Button>
+                <Button
+                    size='small'
+                    disableElevation
+                    variant='contained'
+                    onClick={() => addToCart(item)}
+                >+</Button>
+            </div>
+        </div>
+        <img src={item.image} alt={item.title} />
     </Wrapper>
 );
 
